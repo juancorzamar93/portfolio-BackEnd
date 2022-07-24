7f56bc01d346d9d884bc2e80dd4641028b7491ad
juancz8 <pythonanaconda93@gmail.com>
2022-07-24 01:25:35 -0300

    modificacion

--- src/main/java/com/porfolio/jcz/Controller/CExperiencia.java
+++ src/main/java/com/porfolio/jcz/Controller/CExperiencia.java
@@ -23,7 +23,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("explab")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://project-argentinaprograma-jcz.web.app")
+//@CrossOrigin(origins = "http://localhost:4200")
 public class CExperiencia {
     @Autowired
     SExperiencia sExperiencia;
--- src/main/java/com/porfolio/jcz/Controller/PersonaController.java
+++ src/main/java/com/porfolio/jcz/Controller/PersonaController.java
@@ -16,7 +16,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://project-argentinaprograma-jcz.web.app")
+//@CrossOrigin(origins = "http://localhost:4200")
 public class PersonaController {
     @Autowired IPersonaService ipersonaService;
     
